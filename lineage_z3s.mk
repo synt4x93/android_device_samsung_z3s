eab40e556dca1fb36b1cd41659767f48efc4e595
josip-k <d3hum4niz3d@gmail.com>
2020-12-16 11:19:00 +0100

    z3s: initial commit
    Change-Id: I21b2aeaf9d2967488c4e7dc10c8f840183c2fc14

---
+++ lineage_z3s.mk
@@ -0,0 +1,41 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/z3s/device.mk)
+
+### BOOTANIMATION
+# vendor/lineage/config/common_full_phone.mk
+TARGET_SCREEN_HEIGHT := 3200
+TARGET_SCREEN_WIDTH := 1440
+# vendor/lineage/config/common.mk
+
+### LINEAGE
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_z3s
+PRODUCT_DEVICE := z3s
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G988B
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_PDA_MODEL := G988B
+PRODUCT_PDA_MODEL_VERSION := XXU5CUA3
+PRODUCT_PDA_VERSION := $(PRODUCT_PDA_MODEL)$(PRODUCT_PDA_MODEL_VERSION)
+PRODUCT_SHIPPING_API_LEVEL := 29
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=z3sxxx \
+    PRODUCT_DEVICE=z3s \
+    PRIVATE_BUILD_DESC="z3sxxx-user 11 RP1A.200720.012 $(PRODUCT_PDA_VERSION) release-keys"
+
+BUILD_FINGERPRINT := samsung/z3sxxx/z3s:11/RP1A.200720.012/$(PRODUCT_PDA_VERSION):user/release-keys
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.build.PDA=$(PRODUCT_PDA_VERSION)
+
+ifneq ($(LINEAGE_DEV_CERTIFICATE),)
+    PRODUCT_DEFAULT_DEV_CERTIFICATE := $(LINEAGE_DEV_CERTIFICATE)
+endif
